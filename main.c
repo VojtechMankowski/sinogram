de1e6b6f5db94d99d76950331b0c86f5118b5c6f
VojtechMankowski <vojtaman@gmail.com>
2021-04-28 22:09:16 +0200

    Corrected rotation issue.

--- main.c
+++ main.c
@@ -7,9 +7,9 @@
 
 enum CHANNELS { RED, GREEN, BLUE, ALPHA, NUM_CHANNELS };
 
-void draw_image(unsigned char* data, int width, int height, int channels, enum CHANNELS offset);
+void draw_channel(unsigned char* data, int width, int height, int channels, enum CHANNELS offset);
 
-void rotate_image(unsigned char* data, double angle_deg, int width, int height, int channels, enum CHANNELS offset);
+void rotate_image(unsigned char* rot_data, unsigned char* data, double angle_deg, int width, int height, int channels, enum CHANNELS offset);
 
 void rotate_position(double* x, double* y, int pixel_num, double angle_deg, int width, int height);
 
@@ -22,29 +22,41 @@ int main(void) {
     char * filename = "letters.png";
     unsigned char *data = stbi_load(filename, &width, &height, &channels, 0);
     
+    /* allocate N bytes for rotated image */
+    unsigned char* rot_data = malloc(width*height*channels);
+    memcpy(rot_data, data, width*height*channels);
+
+    /* Alternative to MEMCPY: */
+    // for (int i = 0; i < width*height*channels; i++){
+    //     *(rot_data+i) = *(data+i);
+    // }
+
+    /* check copy */
+    stbi_write_png("copy_check.png", width, height, channels, rot_data, width*channels);
+
     enum CHANNELS offset = RED;
 
-    double angle_deg = -30.0;
+    double angle_deg = -15.0;
     
-    // draw_image(data, width, height, channels, offset);
+    // draw_channel(data, width, height, channels, offset);
 
     for ( int c = 0; c < NUM_CHANNELS; c++ ) {
-        rotate_image(data, angle_deg, width, height, channels, (enum CHANNELS)c);
+        rotate_image(rot_data, data, angle_deg, width, height, channels, (enum CHANNELS)c);
     }
 
     // printf("\n\nROTATED:\n\n");
+    // draw_channel(rot_data, width, height, channels, offset);
 
-    // draw_image(data, width, height, channels, offset);
-
-    stbi_write_png("written.png", width, height, channels, data, width*channels);
+    stbi_write_png("rotated.png", width, height, channels, rot_data, width*channels);
 
     stbi_image_free(data);
+    free(rot_data);
 
     getchar();
     return 0;
 }
 
-void draw_image(unsigned char* data, int width, int height, int channels, enum CHANNELS offset) {
+void draw_channel(unsigned char* data, int width, int height, int channels, enum CHANNELS offset) {
     int N = width*height;
     int pixel_num = 0;
     int val = 0;
@@ -61,7 +73,7 @@ void draw_image(unsigned char* data, int width, int height, int channels, enum C
         }
 }
 
-void rotate_image(unsigned char* data, double angle_deg, int width, int height, int channels, enum CHANNELS offset) {
+void rotate_image(unsigned char* rot_data, unsigned char* data, double angle_deg, int width, int height, int channels, enum CHANNELS offset) {
     int pixel_num, rot_pixel_num;
     int N = width*height;
     double x,y;
@@ -75,7 +87,7 @@ void rotate_image(unsigned char* data, double angle_deg, int width, int height, 
         val = nearest_neighbour(data, x, y, width, height, channels, offset);
 
         // 3. assign value
-        *(data + channels*pixel_num + offset) = val;
+        *(rot_data + channels*pixel_num + offset) = val;
     }
 }
 
@@ -90,16 +102,16 @@ void rotate_position(double* x, double* y, int pixel_num, double angle, int widt
     *y = pixel_num / width;
 
     /* center around middle of image */
-    *x = *x - 1.0*width;
-    *y = *y - 1.0*height;
+    *x = *x - 0.5*width;
+    *y = *y - 0.5*height;
 
     /* compute pixel position after rotation */
     x_rot = (*x) * cos(angle) - (*y) * sin(angle);
     y_rot = (*x) * sin(angle) + (*y) * cos(angle);
     
     /* move origin back to (0,0)*/
-    x_rot = x_rot + 1.0*width;
-    y_rot = y_rot + 1.0*height;
+    x_rot = x_rot + 0.5*width;
+    y_rot = y_rot + 0.5*height;
 
     *x = x_rot;
     *y = y_rot;
@@ -107,10 +119,10 @@ void rotate_position(double* x, double* y, int pixel_num, double angle, int widt
 
 unsigned char nearest_neighbour(unsigned char* data, double x, double y, int width, int height, int channels, enum CHANNELS offset) {
     int pixel_num;
-    unsigned char val;
+    unsigned char val = 0;
 
     if ( x < 0.0 || y < 0.0 || x > (width-1) || y > (height-1) ) {
-        return 0;
+        return val;
     }
 
     x = round(x);
