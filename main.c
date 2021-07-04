466fa7737f1a5ced2c6b1595bf6a99b58da18f01
VojtechMankowski <vojtaman@gmail.com>
2021-07-04 19:18:31 +0200

    Changed structure to create images in loop. Renamed data to input_image.

--- main.c
+++ main.c
@@ -8,71 +8,74 @@
 
 enum CHANNELS { RED, GREEN, BLUE, ALPHA, NUM_CHANNELS };
 
-void draw_channel(unsigned char* data, int width, int height, int channels, enum CHANNELS offset);
+void draw_channel(unsigned char* input_image, int width, int height, int channels, enum CHANNELS offset);
 
-void rotate_image(unsigned char* rot_data, unsigned char* data, double angle_deg, int width, int height, int channels, enum CHANNELS offset);
+void rotate_image(unsigned char* rot_input_image, unsigned char* input_image, double angle_deg, int width, int height, int channels, enum CHANNELS offset);
 
 void rotate_position(double* x, double* y, int pixel_num, double angle_deg, int width, int height);
 
-unsigned char nearest_neighbour(unsigned char* data, double x, double y, int width, int height, int channels, enum CHANNELS offset);
+unsigned char nearest_neighbour(unsigned char* input_image, double x, double y, int width, int height, int channels, enum CHANNELS offset);
 
-unsigned char bilinear_interp(unsigned char* data, double x, double y, int width, int height, int channels, enum CHANNELS offset);
+unsigned char bilinear_interp(unsigned char* input_image, double x, double y, int width, int height, int channels, enum CHANNELS offset);
 
 int main(void) {
-    clock_t start, end;
+    clock_t start_time;
     double cpu_time_used;
     
-    start = clock();
+    start_time = clock();
 
     int width, height, channels;
     char * filename = "letters.png";
-    unsigned char *data = stbi_load(filename, &width, &height, &channels, 0);
-    
-    /* allocate N bytes for rotated image */
-    unsigned char* rot_data = malloc(width*height*channels);
-    memcpy(rot_data, data, width*height*channels);
+    char output_filename[20];
+    unsigned char *input_image = stbi_load(filename, &width, &height, &channels, 0);
+    int angles = 180;
+    enum CHANNELS offset = RED;
+    double angle_deg = 30.0;
 
+    /* Use as a check for small images */
+    // draw_channel(input_image, width, height, channels, offset);
+
+    /* allocate N bytes for rotated image */
+    unsigned char* rot_input_image = malloc(width*height*channels);
+    memcpy(rot_input_image, input_image, width*height*channels);
     /* Alternative to MEMCPY: */
     // for (int i = 0; i < width*height*channels; i++){
-    //     *(rot_data+i) = *(data+i);
+    //     *(rot_input_image+i) = *(input_image+i);
     // }
 
-    /* check copy */
-    stbi_write_png("copy_check.png", width, height, channels, rot_data, width*channels);
+    /* allocate bytes for sinogram image */
+    unsigned char* sinogram = malloc(angles*height*channels);
 
-    enum CHANNELS offset = RED;
+    for (angle_deg = 0.0; angle_deg < angles; angle_deg += 30) {
+        
+        for ( int c = 0; c < NUM_CHANNELS; c++ ) {
+            rotate_image(rot_input_image, input_image, angle_deg, width, height, channels, (enum CHANNELS)c);
+        }
 
-    double angle_deg = -15.0;
-    
-    // draw_channel(data, width, height, channels, offset);
+        sprintf(output_filename, "rotated%d.png", (int)angle_deg);
+        printf("%s\n", output_filename);
 
-    for ( int c = 0; c < NUM_CHANNELS; c++ ) {
-        rotate_image(rot_data, data, angle_deg, width, height, channels, (enum CHANNELS)c);
+        stbi_write_png(output_filename, width, height, channels, rot_input_image, width*channels);
     }
 
-    // printf("\n\nROTATED:\n\n");
-    // draw_channel(rot_data, width, height, channels, offset);
+    stbi_image_free(input_image);
+    free(rot_input_image);
+    free(sinogram);
 
-    stbi_write_png("rotated.png", width, height, channels, rot_data, width*channels);
-
-    stbi_image_free(data);
-    free(rot_data);
-
-    end = clock();
-    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+    cpu_time_used = ((double) (clock() - start_time)) / CLOCKS_PER_SEC;
     printf("Program took %f seconds to execute.\n", cpu_time_used);
 
-    getchar();
+    // getchar();
     return 0;
 }
 
-void draw_channel(unsigned char* data, int width, int height, int channels, enum CHANNELS offset) {
+void draw_channel(unsigned char* input_image, int width, int height, int channels, enum CHANNELS offset) {
     int N = width*height;
     int pixel_num = 0;
     int val = 0;
 
     for (; pixel_num < N; pixel_num++) {
-            val = *(data + channels*pixel_num + offset);
+            val = *(input_image + channels*pixel_num + offset);
             
             if ( pixel_num % width == 0) {
                 printf("\n");
@@ -83,7 +86,7 @@ void draw_channel(unsigned char* data, int width, int height, int channels, enum
         }
 }
 
-void rotate_image(unsigned char* rot_data, unsigned char* data, double angle_deg, int width, int height, int channels, enum CHANNELS offset) {
+void rotate_image(unsigned char* rot_input_image, unsigned char* input_image, double angle_deg, int width, int height, int channels, enum CHANNELS offset) {
     int pixel_num, rot_pixel_num;
     int N = width*height;
     double x,y;
@@ -94,11 +97,11 @@ void rotate_image(unsigned char* rot_data, unsigned char* data, double angle_deg
         rotate_position(&x, &y, pixel_num, angle_deg, width, height);
 
         // 2. compute value (NEAREST)
-        // val = nearest_neighbour(data, x, y, width, height, channels, offset);
-        val = bilinear_interp(data, x, y, width, height, channels, offset);
+        val = nearest_neighbour(input_image, x, y, width, height, channels, offset);
+        // val = bil inear_interp(input_image, x, y, width, height, channels, offset);
 
         // 3. assign value
-        *(rot_data + channels*pixel_num + offset) = val;
+        *(rot_input_image + channels*pixel_num + offset) = val;
     }
 }
 
@@ -128,10 +131,11 @@ void rotate_position(double* x, double* y, int pixel_num, double angle, int widt
     *y = y_rot;
 }
 
-unsigned char nearest_neighbour(unsigned char* data, double x, double y, int width, int height, int channels, enum CHANNELS offset) {
+unsigned char nearest_neighbour(unsigned char* input_image, double x, double y, int width, int height, int channels, enum CHANNELS offset) {
     int pixel_num;
     unsigned char val = 0;
-
+    
+    /* outside image case */
     if ( x < 0.0 || y < 0.0 || x > (width-1) || y > (height-1) ) {
         return val;
     }
@@ -139,35 +143,36 @@ unsigned char nearest_neighbour(unsigned char* data, double x, double y, int wid
     x = round(x);
     y = round(y);
     pixel_num = x + y*width;
-    val = *(data + channels*pixel_num + offset); 
+    val = *(input_image + channels*pixel_num + offset); 
     return val;
 }
 
-unsigned char bilinear_interp(unsigned char* data, double x, double y, int width, int height, int channels, enum CHANNELS offset) {
+unsigned char bilinear_interp(unsigned char* input_image, double x, double y, int width, int height, int channels, enum CHANNELS offset) {
     int pixel_num;
     unsigned char val = 0;
     float val1, val2, val3, val4;
     float val12, val34;
 
+    /* outside image case */
     if ( x < 0.0 || y < 0.0 || x > (width-1) || y > (height-1) ) {
         return val;
     }
-    
+
     /* left top corner */
     pixel_num = floor(x) + floor(y)*width;
-    val1 = (float) *(data + channels*pixel_num + offset);
+    val1 = (float) *(input_image + channels*(pixel_num) + offset);
 
     /* right top corner */
     pixel_num = ceil(x) + floor(y)*width;
-    val2 = (float) *(data + channels*pixel_num + offset);
+    val2 = (float) *(input_image + channels*pixel_num + offset);
     
     /* left bottom corner */
     pixel_num = floor(x) + ceil(y)*width;
-    val3 = (float) *(data + channels*pixel_num + offset);
+    val3 = (float) *(input_image + channels*pixel_num + offset);
     
     /* right bottom corner*/
     pixel_num = ceil(x) + ceil(y)*width;
-    val4 = (float) *(data + channels*pixel_num + offset);
+    val4 = (float) *(input_image + channels*pixel_num + offset);
     
     /* for pixel grid the denominator (x2-x1) = 1 */
     val12 = val1 + (val2-val1)*(x-floor(x));
